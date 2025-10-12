82e248409e7f99d9d406ca14b9e7be328d0c7fad
Kimberly Vargas <kimberly.vargas1723@gmail.com>
2025-10-12 11:08:45 +0000

    DAME UN CHECKLIST COMPLETO DE MEJORAS ESTETICAS INTELIGENCIA Y DE IMPLEM

--- src/components/study-content.tsx
+++
@@ -1,4 +0,0 @@
-// This file is obsolete and will be removed.
-// The logic has been refactored into:
-// - src/components/study-content-container.tsx (for state and logic)
-// - src/components/study-content-display.tsx (for presentation)
