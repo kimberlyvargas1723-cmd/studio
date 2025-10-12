ed8747837f18c756ef4e15a07dc359de3043bf8a
Kimberly Vargas <kimberly.vargas1723@gmail.com>
2025-10-12 02:20:25 +0000

    Try fixing this error: `Build Error: Export useAuth doesn't exist in tar

--- src/firebase/index.ts
+++
@@ -1,5 +0,0 @@
-// src/firebase/index.ts
-'use client';
-// This file can be used to export Firebase-related utilities if needed in the future.
-// For now, it remains empty as we are not using Firebase authentication.
-export {};
